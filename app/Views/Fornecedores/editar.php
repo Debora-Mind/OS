c1e726399fb9e1bd03121da9ccb851b7fd638921
Débora Almeida <debora.almeida.de.mello@gmail.com>
2024-04-13 20:02:56 -0300

    Script viacep removido para um arquivo individual

--- app/Views/Fornecedores/editar.php
+++ app/Views/Fornecedores/editar.php
@@ -46,54 +46,10 @@
     <script src="<?= site_url('recursos/vendor/mask/jquery.mask.min.js') ?>"></script>
 
     <script>
-
-        $('[name=cep]').on('keyup', function () {
-            var cep = $(this).val();
-
-            if (cep.length === 9) {
-                $.ajax({
-                    type: 'GET',
-                    url: '<?= site_url('fornecedores/consultacep') ?>',
-                    data: {
-                        cep: cep
-                    },
-                    dataType: 'json',
-                    beforeSend: function () {
-                        $("#form").LoadingOverlay("show");
-                        $('#cep').html('')
-                    },
-                    success: function (response) {
-                        $("#form").LoadingOverlay("hide", true);
-                        if (!response.erro) {
-                            if (!response.endereco) {
-                                $('[name=endereco').prop('readonly', false);
-                                $('[name=endereco').focus();
-                            } else {
-                                $('[name=endereco').prop('readonly', true);
-                            }
-                            if (!response.bairro) {
-                                $('[name=bairro').prop('readonly', false);
-                            } else {
-                                $('[name=bairro').prop('readonly', true);
-                            }
-
-                            $('[name=endereco]').val(response.endereco);
-                            $('[name=bairro]').val(response.bairro);
-                            $('[name=cidade]').val(response.cidade);
-                            $('[name=estado]').val(response.estado);
-                        } else {
-                            $('#cep').html(response.erro);
-                        }
-                    },
-                    error: function () {
-                        $("#form").LoadingOverlay("hide", true);
-                        alert('Não foi possível processar a solicitação. Por favor entre em contato com o suporte técnico.')
-                    }
-                })
-            }
-        })
-
         $(document).ready(function () {
+
+            <?= $this->include('Fornecedores/_viacep') ?>
+
             $("#form").on('submit', function (e){
 
                 e.preventDefault()
