fa30e399f765afa2dc7bf15e2c6174495bbc77b0
Débora Almeida <debora.almeida.de.mello@gmail.com>
2024-04-14 20:31:19 -0300

    Update Item.php
    Adicionados métodos auxiliares

--- app/Entities/Item.php
+++ app/Entities/Item.php
@@ -7,4 +7,44 @@ use CodeIgniter\Entity\Entity;
 class Item extends Entity
 {
     protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    public function exibeSituacao()
+    {
+        if ($this->deleted_at !== null) {
+            $icone = '<span class="text-white">Excluído</span>&nbsp;<i class="fa fa-undo"></i>&nbsp;Desfazer';
+
+            $situacao = anchor("itens/restaurar/$this->id", $icone, ['class' => 'btn btn-sm']);
+
+            return $situacao;
+        } elseif ($this->ativo) {
+            return '<i class="fa fa-unlock text-success"></i>&nbsp;Ativo';
+        } else {
+            return '<i class="fa fa-lock text-warning"></i>&nbsp;Inativo';
+        }
+    }
+
+    public function exibeTipo()
+    {
+        if ($this->tipo === 'produto') {
+            $tipoItem = '<i class="fa fa-archive text-success"></i>&nbsp;Produto';
+        } else {
+            $tipoItem = '<i class="fa fa-wrench text-white"></i>&nbsp;Serviço';
+        }
+
+        return $tipoItem;
+    }
+
+    public function exibeEstoque()
+    {
+        return $this->tipo === 'produto' ? $this->estoque : 'Não se aplica';
+    }
+
+    public function precoVendaFormatado()
+    {
+        return 'R$&nbsp;' . esc(str_replace('.', ',', $this->preco_venda));
+    }
+    public function precoCustoFormatado()
+    {
+        return 'R$&nbsp;' . esc(str_replace('.', ',', $this->preco_custo));
+    }
 }
