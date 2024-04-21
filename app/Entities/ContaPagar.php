6bbf452173eea3b23a377452cb9a6eb7468c53f2
Débora Almeida <debora.almeida.de.mello@gmail.com>
2024-04-21 11:32:31 -0300

    Create ContaPagar.php

---
+++ app/Entities/ContaPagar.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entities;
+
+use CodeIgniter\Entity\Entity;
+
+class ContaPagar extends Entity
+{
+    protected $dates   = ['created_at', 'updated_at', 'deleted_at'];
+
+    public function exibeSituacao(): string
+    {
+        if ($this->situacao == 1) {
+            return '<i class="fa fa-check-circle text-success">
+                    </i>&nbsp;Conta foi paga em ' .
+                    date('d/m/Y', strtotime($this->updated_at));
+        }
+
+        if ($this->data_vencimento == date('Y-m-d')) {
+            return '<i class="fa fa-info-circle text-warning">
+                    </i>&nbsp;Conta vencerá hoje ' .
+                    date('d/m/Y', strtotime($this->data_vencimento));
+        }
+
+        if ($this->data_vencimento > date('Y-m-d')) {
+            return '<i class="fa fa-info-circle text-info">
+                    </i>&nbsp;Conta vencerá em ' .
+                date('d/m/Y', strtotime($this->data_vencimento));
+        }
+
+        if ($this->data_vencimento < date('Y-m-d') && $this->situacao == 0) {
+            return '<i class="fa fa-exclamation-circle text-danger">
+                    </i>&nbsp;Conta venceu em ' .
+                date('d/m/Y', strtotime($this->data_vencimento));
+        }
+
+        return '<i class="fa fa-exclamation-circle text-danger">
+                    </i>&nbsp;Não encontramos os dados da sua conta';
+    }
+}
