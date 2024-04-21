80625b8b60a4eac82b57e08a720b06e3a8ead4ed
Débora Almeida <debora.almeida.de.mello@gmail.com>
2024-04-21 16:01:54 -0300

    Update Routes.php
    Criado grupo de rotas para contas a pagar

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,3 +11,10 @@ $routes->get('/', 'Home::index');
 $routes->get('login', 'Login::novo');
 $routes->get('logout', 'Login::logout');
 $routes->get('esqueci', 'Password::esqueci');
+
+
+$routes->group('contas', function($routes)
+{
+    $routes->add('/', 'ContasPagar::index');
+    $routes->add('recuperacontas', 'ContasPagar::recuperaContas');
+});
